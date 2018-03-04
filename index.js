f9c51822ce64f7a61c9a7f1da51a14cff36c40bc
Ioane Sharvadze <ioane.sharvadze@gmail.com>
2018-03-04 19:51:23 +0200

    fix server

--- index.js
+++ index.js
@@ -15,38 +15,39 @@ io.sockets.on('connection', function (socket) {
     /**
      * Client Requests to Join/Create it's own room
      */
-    socket.on('enter_my_room', function (publicKey) {
-        log(publicKey + ' requests to enter room');
+    socket.on('enter_my_room', function (message) {
+        log(message);
+        log(message.fromPublicKey + ' requests to enter room');
         // TODO maybe this room object will be useful in the future.
-        var room = io.sockets.adapter.rooms[publicKey];
+        let room = io.sockets.adapter.rooms[message.fromPublicKey];
         // TODO add authentication logic
         if (false) {
             socket.emit('error', 'authentication error', socket.id);
         }
 
-        socket.join(publicKey);
+        socket.join(message.fromPublicKey);
         socket.emit('enter_my_room', true);
     });
 
-    socket.on('connection_request', function (senderPublicKey, recipientPublicKey) {
-        log('connection_request: from : ' + senderPublicKey + ' to: ' + recipientPublicKey);
-        var room = io.sockets.adapter.rooms[recipientPublicKey];
+    socket.on('connection_request', function (message) {
+        log('connection_request: from : ' + message.fromPublicKey + ' to: ' + message.toPublicKey);
+        let room = io.sockets.adapter.rooms[message.toPublicKey];
         console.log('sending message to room: ' + room + ' length: ' + room.length);
-        io.to(recipientPublicKey).emit('connection_request', senderPublicKey);
+        io.to(message.toPublicKey).emit('connection_request', message);
     });
 
     /**
      * In this case server will pass out message from this socket to recipients room.
      */
-    socket.on('signalling_message', function (recipientPublicKey, message) {
-        log('signalling_message: ' + recipientPublicKey);
-        var room = io.sockets.adapter.rooms[recipientPublicKey];
+    socket.on('signalling_message', function (message) {
+        log('signalling_message: to: ' + message.toPublicKey);
+        let room = io.sockets.adapter.rooms[message.toPublicKey];
         console.log('sending message to room: ' + room + ' length: ' + room.length);
-        io.to(recipientPublicKey).emit('signalling_message', message);
+        io.to(message.toPublicKey).emit('signalling_message', message);
     });
 
     socket.on('ipaddr', function () {
-        var ifaces = os.networkInterfaces();
+        let ifaces = os.networkInterfaces();
         for (var dev in ifaces) {
             ifaces[dev].forEach(function (details) {
                 if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
